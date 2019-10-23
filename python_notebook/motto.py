8e89053814dfd3ff45e0933e301adacb19089c15
Administrator <707148142@qq.com>
2019-10-23 23:50:46 +0800

    删除垃圾文件，修改propety

--- python_notebook/motto.py
+++
@@ -1,7 +0,0 @@
-print("go big or go home")
-
-str1='this is A'
-print(str1)
-
-str2='this is B'
-print(str2)
--- python_notebook/property.py
+++ python_notebook/property.py
@@ -19,11 +19,32 @@
 #结果：TypeError: 'NoneType' object is not callable
 #因为eat此时已经变成一个属性了， 不是方法了， 想调用已经不需要加()号了，直接d.eat就可以了
 
-class Person(object):
-    def __init__(self, name):
-        self.name = name
-    @property  # 把eat方法变为属性方法
-    def eat(self):
-        print("%s is eating" % self.name)
-d = Person("xiaoming")
-d.eat
+# class Person(object):
+#     def __init__(self, name):
+#         self.name = name
+#     @property  # 把eat方法变为属性方法
+#     def eat(self):
+#         print("%s is eating" % self.name)
+# d = Person("xiaoming")
+# d.eat
+
+
+class C:
+    def __init__(self,size=10):
+        self.size = size
+
+    def getSize(self):
+        return self.size
+
+    def setSize(self,vaule):
+        self.size = vaule
+
+    def delSize(self):
+
+        del self.size
+
+    x=property(getSize,setSize,delSize)
+
+c =C()
+
+c.x
