2872a10ac1c55969e121962a01b699e37dd4a8c5
Yuxichen <xichen.yu@sohan.hk>
2019-10-23 09:07:50 +0800

    测试GIT push3

---
+++ python_notebook/switch_to.alert().py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*- 
+# 2019/10/22 18:01 
+# Test
+# super_1.py 
+# hanwenlu
+
+#警告框处理
+#text:返回alert confirm prompt中的文字信息
+#accept（）：接受现有警告框。
+#dismiss():解散现有警告框
+#send_keys():
+
+from time import sleep
+
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+
+dr = webdriver.Chrome()
+dr.get('https://www.baidu.com')
+dr.maximize_window()
+
+above = dr.find_element_by_link_text('设置')
+#采用鼠标悬停
+ActionChains(dr).move_to_element(above).perform()
+dr.find_element_by_link_text('搜索设置').click()
+sleep(2)
+
+dr.find_element_by_class_name('prefpanelgo').click()
+
+sleep(3)
+alret = dr.switch_to.alert
+
+alret_text = alret.text
+
+print(alret_text)
+
+alret.accept()
+
+dr.quit()
