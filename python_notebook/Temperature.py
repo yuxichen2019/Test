f35b5617fb6bdcc045b933d0d530bc0283ae1197
Administrator <707148142@qq.com>
2019-10-25 00:33:45 +0800

    描述符相关的魔法方法（温度转换）

--- python_notebook/Temperature.py
+++ python_notebook/Temperature.py
@@ -5,7 +5,9 @@
 # 文件名：Temperature.py
 # 开发工具：PyCharm
 
-#描述符
+#描述符相关的魔法方法
+#给摄氏度赋值  自动计算出华氏度
+#描述符  摄氏度
 class Celsius:
     def __init__(self, value=37):
         self.value = float(value)
@@ -13,17 +15,23 @@ class Celsius:
         return self.value
     def __set__(self, instance, value):
         self.value = float(value)
-
+#描述符 华氏度
 class Fahrenheit:
+    # self是自己的实例：fah。  instance是拥有者Temperature的实例temp。  owner就是Temperature
     def __get__(self, instance, owner):
         return instance.cel * 1.8 + 32
     def __set__(self, instance, value):
         instance.cel = (float(value) - 32) / 1.8
-
+#温度类
 class Temperature:
     cel = Celsius()
     fah = Fahrenheit()
 
 temp = Temperature()
-print(temp.cel)
+#设置摄氏度30  求华氏度
+temp.cel=30
 print(temp.fah)
+
+#华氏度86  求摄氏度
+temp.fah = 86
+print(temp.cel)
