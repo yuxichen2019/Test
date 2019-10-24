f1c5c471c3533f3d54d2e9681afc1423c6f4361d
Yuxichen <xichen.yu@sohan.hk>
2019-10-24 16:27:38 +0800

    开枪

---
+++ python_notebook/face_class.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*- 
+# 2019/10/24 11:40 
+# Test
+# face_class.py 
+# hanwenlu
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Bullet(object):
+    def __init__(self,temp_power):
+            super(Bullet,self).__init__()
+            self.power = temp_power
+
+    def
