6a18f1af621b7c061da4b83b96f5d80f780d4760
Administrator <707148142@qq.com>
2019-10-24 21:33:29 +0800

    温度切换

--- python_notebook/Temperature.py
+++ python_notebook/Temperature.py
@@ -6,11 +6,22 @@
 # 开发工具：PyCharm
 
 class Celsius:
-    def __init__(self,value=37):
+    def __init__(self, value=37):
         self.value = float(value)
 
     def __get__(self, instance, owner):
-        return  self.value
+        return self.value
 
     def __set__(self, instance, value):
+        self.value = float(value)
 
+class Fahrenheit:
+    def __get__(self, instance, owner):
+        return instance.cel * 1.8 + 32
+
+    def __set__(self, instance, value):
+        instance.cel = (float(value) - 32) / 1.8
+
+class Temperature:
+    cel = Celsius()
+    fah = Fahrenheit()
