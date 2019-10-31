c7b77e8ba7b4b183cecb31e7c573142003d81849
Yuxichen <xichen.yu@sohan.hk>
2019-10-31 18:16:20 +0800

    cookie

--- selenium/Cookie.py
+++ selenium/Cookie.py
@@ -10,18 +10,16 @@ dr = webdriver.Chrome()
 dr.get("http://www.baidu.com")
 
 
-cookie = dr.get_cookies()
-print(cookie)
+cookies = dr.get_cookies()
+print(cookies)
 
 
 #添加cookie信息
 dr.add_cookie({'name':'key_aaaaaaaaa','value':'key_bbbbbbbbbbbb'})
-
+print(dr.get_cookies())
 
 #遍历指定的Cookies
 for cookie in dr.get_cookies():
-
-
     print('%s ----->  %s' % (cookie['name'], cookie['value']))
 
 dr.quit()
