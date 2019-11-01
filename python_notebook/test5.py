625d2822d2ead2a262eb005f765c73b001db5e4c
Administrator <707148142@qq.com>
2019-11-02 03:26:06 +0800

    1

--- python_notebook/test5.py
+++ python_notebook/test5.py
@@ -48,3 +48,49 @@ def myadd(a,b):
 if __name__ ==  '__main__':
     myadd(Point(1,2),Point(3,4)).info()
     myadd(D3Point(1,2,3),D3Point(4,5,6)).info()
+
+
+
+
+
+
+
+
+
+
+
+class SingleClass:
+    def __init__(self,x = 0):
+        self.x = 0
+
+sc = SingleClass()
+
+def tsc():
+    print(sc.x)
+    sc.x = 10
+    print(sc.x)
+
+def tsc2():
+    print(sc.x)
+    sc.x = 8
+    print(sc.x)
+
+if  __name__ == "__main__":
+    tsc()
+    tsc2()
+
+
+
+
+class Singleton:
+    def __new__(cls,*args,**kwargs):
+        if not hasattr(cls,'_sgl'):
+            cls._sgl = super().__new__(cls,*args,**kwargs)
+
+        return cls._sgl
+
+if __name__ == '__main__':
+    sa = Singleton()
+    sb = Singleton()
+    print(id(sa))
+    print(id(sb))
