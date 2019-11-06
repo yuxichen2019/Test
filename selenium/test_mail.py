2213504d07dcc2c763730d18d562027996b3ba83
Yuxichen <xichen.yu@sohan.hk>
2019-11-06 18:25:53 +0800

    模块化与参数化

---
+++ selenium/test_mail.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*- 
+# 2019/11/06 10:16 
+# Test
+# test_mail.py 
+# company
+
+from time import sleep
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+driver.get("http://www.126.com")
+
+sleep(2)
+driver.find_element_by_link_text('密码登录').click()
+
+lg = driver.find_element_by_css_selector('iframe[id^="x-URS-iframe"]')
+
+driver.switch_to.frame(lg)
+driver.find_element_by_name('email').clear()
+driver.find_element_by_name('email').send_keys('Y15112342277')
+driver.find_element_by_name('password').clear()
+driver.find_element_by_name('password').send_keys('yxc123456')
+driver.find_element_by_id('dologin').click()
+sleep(1)
+#driver.find_element_by_xpath('//*[@id="_mail_link_6_152"]')
+
+
+driver.find_element_by_link_text('退出').click()
+
+driver.quit()
