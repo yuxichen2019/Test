2e20681b1b46f2301ead98dbad379fa270e476cb
Administrator <707148142@qq.com>
2019-11-07 22:45:48 +0800

    邮箱登录 退出

--- selenium/test_mail.py
+++ selenium/test_mail.py
@@ -10,12 +10,15 @@ from module import Mail
 
 driver = webdriver.Chrome()
 driver.get("http://www.126.com")
+driver.maximize_window()
+driver.find_element_by_link_text('密码登录').click()
 
 mail = Mail(driver)
 mail.login()
 
-sleep(4)
+sleep(2)
 mail.logout()
+sleep(1)
 driver.quit()
 
 
@@ -24,18 +27,19 @@ driver.quit()
 
 
 # sleep(2)
-# driver.find_element_by_link_text('密码登录').click()
+# river.find_element_by_link_text('密码登录').click()d
 # lg = driver.find_element_by_css_selector('iframe[id^="x-URS-iframe"]')
 # driver.switch_to.frame(lg)
+#
 # driver.find_element_by_name('email').clear()
 # driver.find_element_by_name('email').send_keys('Y15112342277')
 # driver.find_element_by_name('password').clear()
 # driver.find_element_by_name('password').send_keys('yxc123456')
 # driver.find_element_by_id('dologin').click()
 # sleep(1)
-# #driver.find_element_by_xpath('//*[@id="_mail_link_6_152"]')
-#
-#
+#driver.find_element_by_xpath('//*[@id="_mail_link_6_152"]')
+
+
 # driver.find_element_by_link_text('退出').click()
 
 # driver.quit()
