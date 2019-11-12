ba58050a42756a97e79b9f87a56faaf4aa55b275
Administrator <707148142@qq.com>
2019-11-12 20:51:50 +0800

    文件夹要加source 才能别的地方调用

--- selenium/test_case/leap_year.py
+++ selenium/test_case/leap_year.py
@@ -7,12 +7,12 @@
 
 #判断某年是否为闰年
 class LeapYear:
-    def __init__(self,year):
+    def __init__(self, year):
         self.year = int(year)
 
     def answer(self):
         year = self.year
-        if year%100 == 0 :
+        if year % 100 == 0:
             if year % 400 == 0:
                 return "{0}是闰年".format(year)
 
@@ -24,4 +24,3 @@ class LeapYear:
                 return "{0}是闰年".format(year)
             else:
                 return "{0}不是闰年".format(year)
-
