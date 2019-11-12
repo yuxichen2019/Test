c831cf6be90a3273c4974017f29d99189b0dc097
Administrator <707148142@qq.com>
2019-11-12 23:54:43 +0800

    模块

--- python_notebook/m1.py
+++ python_notebook/m1.py
@@ -5,6 +5,9 @@
 # 文件名：m1.py
 # 开发工具：PyCharm
 
+#请先按步骤将P13_7.py放在文件夹M1中
 import M1.p13_7 as tc
 print("32摄氏度= %.2f华氏度" % tc.c2f(32))
-print("99华氏度= %.2f摄氏度" % tc.f2c(32))
+print("99华氏度= %.2f摄氏度" % tc.f2c(99))
+
+#这里M1文件夹 没有 make dir as sources root
