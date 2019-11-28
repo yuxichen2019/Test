278d07078f76846efeb01075c2271fe0e3b93021
Administrator <707148142@qq.com>
2019-11-28 21:41:05 +0800

    参数化，

--- selenium/test_baidu/test_baidu.py
+++ selenium/test_baidu/test_baidu.py
@@ -4,11 +4,10 @@
 # test_baidu.py 
 # company
 
-
 import unittest
 from time import sleep
 from selenium import webdriver
-from parametrized import parametrized
+from parameterized import parameterized
 
 class TestBaidu(unittest.TestCase):
     @classmethod
@@ -22,17 +21,18 @@ class TestBaidu(unittest.TestCase):
         self.driver.find_element_by_id('su').click()
         sleep(3)
 
-    @parametrized.expand([
+    @parameterized.expand([
         ("case1","selenium"),
         ("case2","unittest"),
-        ("case3","parametrized"),
+        ("case3","parameterized"),
         ])
-        def test_search(self,name,search_key):
+    def test_search(self,name,search_key):
         self.baidu_search(search_key)
         self.assertEqual(self.driver.title,search_key + '_百度搜索')
 
     @classmethod
     def tearDownClass(cls):
         cls.driver.quit()
+
 if __name__ == '__main__':
     unittest.main(verbosity=2)
